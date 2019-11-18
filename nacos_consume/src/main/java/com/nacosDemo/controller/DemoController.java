f45ece486dbeaa5fb9338043bdf0324994c1eb6c
liyang <liyang@126.com>
2019-11-18 17:11:58 +0800

    增加nacos zuul配置

--- nacos_consume/src/main/java/com/nacosDemo/controller/DemoController.java
+++ nacos_consume/src/main/java/com/nacosDemo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.nacosDemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/nacosConsumeDemo")
+@RefreshScope
 public class DemoController {
+
+    @Value("${refresh.name}")
+    private String refreshName;
+
     @GetMapping("/firstDemo/{string}")
     public String firstDemo(@PathVariable(value = "string") String string){
+        System.out.println("refreshName====================" + refreshName);
         return string;
     }
 }
