797fd7e09eceb6620b96b310f43f4080283656fd
DESKTOP-OFCFKJG\win10 <albertchenduo@hotmail.com>
2020-02-28 11:33:53 +0800

    修改洽谈项目分页查询sql

--- src/main/java/com/bit/module/manager/dao/EnquiryAuditDao.java
+++ src/main/java/com/bit/module/manager/dao/EnquiryAuditDao.java
@@ -1,6 +1,7 @@
 package com.bit.module.manager.dao;
 
 import com.bit.module.manager.bean.EnquiryAudit;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 @Repository
 public interface EnquiryAuditDao {
 
-    void batchAdd(List<EnquiryAudit> list );
+    void batchAdd(@Param(value = "list") List<EnquiryAudit> list );
 
 	/**
 	 * 新增记录
