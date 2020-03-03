f4ad21ba41f029e5bd71fd80d12f8f5a4871311f
liyang <liyang@126.com>
2020-03-03 17:29:53 +0800

    default 接口测试

---
+++ nacos_consume/src/test/java/baseTest/bastTest.java
@@ -0,0 +1,9 @@
+package baseTest;
+
+/**
+ * @description:
+ * @author: liyang
+ * @date: 2020-03-02
+ **/
+public class bastTest {
+}
---
+++ nacos_consume/src/test/java/defaultInteface/ServiceA.java
@@ -0,0 +1,9 @@
+package defaultInteface;
+
+/**
+ * @description:
+ * @author: liyang
+ * @date: 2020-03-03
+ **/
+public interface ServiceA {
+}
