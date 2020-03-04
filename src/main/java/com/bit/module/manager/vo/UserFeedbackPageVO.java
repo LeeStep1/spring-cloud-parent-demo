cfbe454df12e29bb07539d8fe43b892dac107a68
lyj <tj_lyj1@126.com>
2020-03-04 15:35:12 +0800

    [新版本功能]增加时间参数

--- src/main/java/com/bit/module/manager/vo/UserFeedbackPageVO.java
+++ src/main/java/com/bit/module/manager/vo/UserFeedbackPageVO.java
@@ -1,7 +1,10 @@
 package com.bit.module.manager.vo;
 
 import com.bit.base.vo.BasePageVo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -36,4 +39,17 @@ public class UserFeedbackPageVO  extends BasePageVo implements Serializable {
      * 创建时间
      */
     private Date createTime;
+
+    /**
+     * 开始时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date beginTime;
+
+    /**
+     * 结束时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date endTime;
+
 }
